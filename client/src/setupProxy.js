73cc533ea0ddbeb6593cc58c534fa5f12e4138dd
Yaser <yaser.azem@otmail.com>
2020-04-10 11:31:15 +0300

    start stripe payment integration

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 const server_port = require('../../config/glob-config');
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(proxy('/auth/google', { target: 'http://localhost:5000' }));
   app.use(proxy('/auth/facebook', { target: 'http://localhost:5000' }));
   app.use(proxy('/api/*', { target: 'http://localhost:5000' }));
@@ -17,7 +17,7 @@ module.exports = function(app) {
   );
   app.use(
     proxy('/api/products/user/*', {
-      target: 'http://localhost:5000' + server_port,
+      target: 'http://localhost:5000',
     }),
   );
 };
