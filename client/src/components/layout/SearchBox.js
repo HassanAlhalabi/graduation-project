95ccdfa914e51b397443f68725fa96d8dc5ea510
Hassan Alhalabi <williamblacksmith@outlook.com>
2020-04-14 18:57:44 +0300

    Add microphone,image button to search bar

--- client/src/components/layout/SearchBox.js
+++ client/src/components/layout/SearchBox.js
@@ -4,16 +4,30 @@ class SearchBox extends Component {
     render(){
         return(
             <div className='search-box mt-md-4'>
-                <form className='form-inline position-relative'>  
-                    <input 
-                        type='search'
-                        placeholder='Search products ...' 
-                        className='form-control rounded-0 w-100 p-3' 
-                    />
-                    <button type='submit' className='rounded-0 position-absolute'>
-                        <i className='fas fa-search'></i>
-                    </button>
-                </form>
+                <form className='form-inline'> 
+                    <div className='position-relative flex-grow-1'>
+                        
+                            <input 
+                                type='search'
+                                placeholder='Search products ...' 
+                                className='form-control rounded-0 w-100 p-3' 
+                            />
+                            <button type='submit' className='rounded-0 position-absolute'>
+                                <i className='fas fa-search'></i>
+                            </button>
+                        
+                    </div>    
+                    <div>
+                        <button type='submit' className='rounded-0'>
+                            <i className='fas fa-microphone'></i>
+                        </button>
+                    </div>
+                    <div>
+                        <button type='submit' className='rounded-0'>
+                            <i className='fas fa-image'></i>
+                        </button>
+                    </div>
+                </form> 
             </div>
         );
     }
