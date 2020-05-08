e297c93d47cd346640afeec226ff83b9c2418132
Hassan Alhalabi <williamblacksmith@outlook.com>
2020-05-08 23:37:07 +0300

    Update about us page

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -13,59 +13,75 @@ const About  = () =>
     <CategoriesBar />
     <Breadcrumb page={'About'}/>
 
-      <div className="about-info container">
-        <h2 className="text-center mt-5 mb-5 about-header">About Our Store</h2>
-        <div className="text-weight-bold">
-          <div className='row  mb-5'>
-            <div className='col-12 col-md-6'>
-              <h4 className='pt-4 pb-4'>Lorem ipsum dolor</h4>
-              <p className='pr-5 mb-4'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut
-                justo in mauris lobortis bibendum nec nec tellus. Morbi est ipsum,
-                eu euismod turpis,
-                eget ornare nunc. Suspendisse id aliquet justo, eget ornare ligula.
-                Nunc eu pretium nisi. 
-              </p>
-              <p>
-                vel maximus leo. Ut tempus risus quis
-                ultricies dictum. Donec at mauris justo. Vestibulum ut lorem vitae
-                tellus facilisis volutpat vitae quis purus. Suspendisse lobortis
-                fermentum placerat.
-              </p> 
-            </div>
-            <div className='col-12 col-md-6'>
-              <div className='img-holder'>
-                <img src={AboutImg1} alt="about-img" className='img-fluid'/>
-              </div>
-            </div>
-            
+      <section className='about-section-1 position-relative'>
+
+        <header className='position-relative'>
+          <div className='container'>
+            <h2 className="about-header">About Our Store</h2>
           </div>
-          <div className='row  mb-5'>
-          <div className='col-12 col-md-6'>
-              <div className='img-holder'>
-                <img src={AboutImg2} alt="about-img" className='img-fluid'/>
+        </header>
+
+        <div className="about-info container">
+          <div className="text-weight-bold">
+            <div className='row  mb-5'>
+              <div className='col-12 col-md-6'>
+                <h4 className='pt-4 pb-4'>Lorem ipsum dolor</h4>
+                <p className='pr-5 mb-4'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut
+                  justo in mauris lobortis bibendum nec nec tellus. Morbi est ipsum,
+                  eu euismod turpis,
+                  eget ornare nunc. Suspendisse id aliquet justo, eget ornare ligula.
+                  Nunc eu pretium nisi. 
+                </p>
+                <p>
+                  vel maximus leo. Ut tempus risus quis
+                  ultricies dictum. Donec at mauris justo. Vestibulum ut lorem vitae
+                  tellus facilisis volutpat vitae quis purus. Suspendisse lobortis
+                  fermentum placerat.
+                </p> 
+              </div>
+              <div className='col-12 col-md-6'>
+                <div className='img-holder'>
+                  <img src={AboutImg1} alt="about-img" className='img-fluid'/>
+                </div>
               </div>
             </div>
-            <div className='col-12 col-md-6 mb-5'>
-              <h4 className='pt-4 pb-4 pl-md-5'>Lorem ipsum dolor</h4>
-              <p className='pl-md-5 mb-4'>
-                Nam efficitur dui et urna ullamcorper rhoncus. Proin porta aliquet
-                ipsum, ac volutpat diam auctor eu. Ut tristique odio ligula, ut
-                In vel magna a velit rhoncus ornare.
-                Phasellus arcu felis, consectetur non nunc nec, finibus rutrum
-                libero. Nulla consequat lorem et felis semper, ut tempus orci
-                lacinia.
-              </p>  
-              <p className='pl-md-5'>
-                Nam blandit et metus
-                ut vestibulum. Maecenas bibendum diam laoreet urna sollicitudin, in
-                pellentesque diam condimentum. 
-              </p>
+          </div>
+        </div>  
+      </section> 
+
+      <section className='about-section-2 position-relative pb-5'>
+        <div className="about-info container">
+          <div className="text-weight-bold">
+            <div className='row'>
+              <div className='col-12 col-md-6'>
+                  <div className='img-holder'>
+                    <img src={AboutImg2} alt="about-img" className='img-fluid'/>
+                  </div>
+                </div>
+                <div className='col-12 col-md-6'>
+                  <h4 className='pt-4 pb-4 pl-md-5'>Lorem ipsum dolor</h4>
+                  <p className='pl-md-5 mb-4'>
+                    Nam efficitur dui et urna ullamcorper rhoncus. Proin porta aliquet
+                    ipsum, ac volutpat diam auctor eu. Ut tristique odio ligula, ut
+                    In vel magna a velit rhoncus ornare.
+                    Phasellus arcu felis, consectetur non nunc nec, finibus rutrum
+                    libero. Nulla consequat lorem et felis semper, ut tempus orci
+                    lacinia.
+                  </p>  
+                  <p className='pl-md-5'>
+                    Nam blandit et metus
+                    ut vestibulum. Maecenas bibendum diam laoreet urna sollicitudin, in
+                    pellentesque diam condimentum. 
+                  </p>
+                </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
+
     </div>
+
 
 
 export default About;
--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     let year;
     year = 2019
     return(
-        <footer className='pt-4 pb-4 mt-5'>
+        <footer className='pt-4 pb-4'>
             <div className='container'>
                 <div className='upper-footer'>
                     <div className='row'>
